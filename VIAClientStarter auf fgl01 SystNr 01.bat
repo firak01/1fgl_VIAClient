8970c3e8084c197be912258d6c415050f60cb50f
firak01 <firak01@t-online.de>
2019-02-10 07:10:51 +0100

    VIA Client Externe Konfigurationsdateien
    Signed-off-by: firak01 <firak01@t-online.de>

---
+++ VIAClientStarter auf fgl01 SystNr 01.bat
@@ -0,0 +1,1 @@
+java -jar c:\java\VIAClient\VIAClientServer_fat.jar -k VIA -d c:\java\VIAClient -f ZKernelConfigVideoArchiveClientFGL01.ini
---
+++ VIAClientStarter auf fgl02 SystNr 02.bat
@@ -0,0 +1,1 @@
+java -jar c:\1fgl\client\VIAClient\VIAClientServer_fat.jar -k VIA -s 02 -d c:\1fgl\client\VIAClient -f ZKernelConfigVideoArchiveClientFGL02.ini
