98e57b196ac667fdd72e996347adf3d4754acccd
firak01 <firak01@t-online.de>
2019-02-27 20:53:58 +0100

    weiter aufräumen
    Signed-off-by: firak01 <firak01@t-online.de>

--- VIAClientStarter auf fgl01 SystNr 01.bat
+++
@@ -1,3 +0,0 @@
-java -jar c:\java\VIAClient\VIAClientServer_fat.jar -k VIA -d c:\java\VIAClient -f ZKernelConfigVideoArchiveClientFGL01.ini
-pause
-exit
---
+++ aufgeraeumte_alte_Dateien/VIAClientStarter auf fgl01 SystNr 01.bat
@@ -0,0 +1,3 @@
+java -jar c:\java\VIAClient\VIAClientServer_fat.jar -k VIA -d c:\java\VIAClient -f ZKernelConfigVideoArchiveClientFGL01.ini
+pause
+exit
